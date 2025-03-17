3a5c0723f64cc0e31499dc75903e85067c0a1167
Maks3w <github.maks3w@virtualplanets.net>
2025-03-17 10:56:32 +0100

    Carousel image component

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,6 +1,7 @@
-import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import {Carousel as ResponsiveCarousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Carousel.module.css';
+import Image from 'next/image';
 
 interface CarouselProps {
     images: string[];
@@ -8,14 +9,17 @@ interface CarouselProps {
     onClose: () => void;
 }
 
-const Carousel = ({ images, initialIndex, onClose }: CarouselProps) => {
+const Carousel = ({images, initialIndex, onClose}: CarouselProps) => {
     return (
         <div className={styles.carouselContainer}>
             <button onClick={onClose} className={styles.closeButton}>Close</button>
             <ResponsiveCarousel selectedItem={initialIndex} showThumbs={false} infiniteLoop useKeyboardArrows>
                 {images.map((image, index) => (
                     <div key={index} className={styles.imageContainer}>
-                        <img src={image} alt={`Slide ${index + 1}`} className={styles.image} loading="lazy" />
+                        <Image src={image} alt={`Slide ${index + 1}`}
+                               width={800}
+                               height={600}
+                               className={styles.image} loading="lazy"/>
                     </div>
                 ))}
             </ResponsiveCarousel>
