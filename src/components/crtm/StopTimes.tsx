eebf90ccb1431a704360b9923f9cb86eaac46841
Maks3w <github.maks3w@virtualplanets.net>
2025-03-17 23:04:23 +0100

    Show error message if crtm fails

--- src/components/crtm/StopTimes.tsx
+++ src/components/crtm/StopTimes.tsx
@@ -5,21 +5,26 @@ interface StopTimesProps {
 }
 
 export default async function StopTimes({stopId}: StopTimesProps) {
-  const stopTimes = await getStopsTimes(stopId);
-  const lineStatuses = stopTimes.linesStatus.LineStatus;
-  const lineNames = lineStatuses.map((lineStatus) => lineStatus.line.shortDescription);
-  const times= stopTimes.times.Time || [];
+  try {
+    const stopTimes = await getStopsTimes(stopId);
+    const lineStatuses = stopTimes.linesStatus.LineStatus;
+    const lineNames = lineStatuses.map((lineStatus) => lineStatus.line.shortDescription);
+    const times = stopTimes.times.Time || [];
 
-  return (
-    <div>
-      {times.map((time) => (
-        <div key={time.codVehicle}>
-          <div>{time.time} - {time.line.shortDescription} - {time.destination}</div>
-        </div>
-      ))}
-      {times.length === 0 && (
-        <div>No hay tiempos de paso para las lineas {lineNames.join(', ')}</div>
-      )}
-    </div>
-  );
+    return (
+      <div>
+        {times.map((time) => (
+          <div key={time.codVehicle}>
+            <div>{time.time} - {time.line.shortDescription} - {time.destination}</div>
+          </div>
+        ))}
+        {times.length === 0 && (
+          <div>No hay tiempos de paso para las lineas {lineNames.join(', ')}</div>
+        )}
+      </div>
+    );
+  } catch (error) {
+    console.error("Failed to fetch stop times:", error);
+    return <div>Error fetching stop times. Please try again later.</div>;
+  }
 }
