fafbd902918b3d4bdb1c9c5cd7e4a84aa18f7ffb
Maks3w <github.maks3w@virtualplanets.net>
2025-03-17 23:41:40 +0100

    CRTM timeout 9s

--- src/components/crtm/widgetApi.ts
+++ src/components/crtm/widgetApi.ts
@@ -1,13 +1,15 @@
 import {GetStopsInformationResponse, GetStopsTimesResponse, Stop, StopTimes} from "@/components/crtm/types/Stop";
 
+const TIMEOUT = 9000;
+
 export const getStopsInformation = async (stopId: string): Promise<Stop> => {
-  const response = await fetch(`https://www.crtm.es/widgets/api/GetStopsInformation.php?codStop=${stopId}`);
+  const response = await fetch(`https://www.crtm.es/widgets/api/GetStopsInformation.php?codStop=${stopId}`, { signal: AbortSignal.timeout(TIMEOUT) });
   const payload: GetStopsInformationResponse = await response.json();
   return payload.stops.StopInformation;
 }
 
 export const getStopsTimes = async (stopId: string): Promise<StopTimes> => {
-  const response = await fetch(`https://www.crtm.es/widgets/api/GetStopsTimes.php?codStop=${stopId}&type=1&orderBy=2&stopTimesByIti=3`);
+  const response = await fetch(`https://www.crtm.es/widgets/api/GetStopsTimes.php?codStop=${stopId}&type=1&orderBy=2&stopTimesByIti=3`, { signal: AbortSignal.timeout(TIMEOUT) });
   const payload: GetStopsTimesResponse = await response.json();
   // parse the time
   const {Time} = payload.stopTimes.times;
