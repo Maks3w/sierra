576dcd16641678c1a5ed6b7f3fb45c65a6232f12
Maks3w <github.maks3w@virtualplanets.net>
2025-03-17 11:19:01 +0100

    Fix useSearchParams() should be wrapped in a suspense boundary at page

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -1,40 +1,14 @@
-"use client"
-
-import {redirect, useSearchParams} from 'next/navigation';
 import Carousel from '@/components/Carousel';
-import places from '@/config/placesConfig';
-import {useEffect, useState} from "react";
+import {Suspense} from "react";
 
 const CarouselPage = () => {
-  const searchParams = useSearchParams();
-  const place = searchParams.get('place');
-  const index = searchParams.get('index');
-  const [initialIndex, setInitialIndex] = useState(0);
-  const [images, setImages] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (place && index !== undefined) {
-      const selectedPlace = places.find(p => p.name === place);
-      if (selectedPlace) {
-        setInitialIndex(Number(index));
-        setImages(selectedPlace.webcams.map(webcam => webcam.url));
-      }
-    }
-  }, [place, index]);
-
-  const handleClose = () => {
-    redirect('/');
-  };
-
-  return (
-    <div>
-      <Carousel
-        images={images}
-        initialIndex={initialIndex}
-        onClose={handleClose}
-      />
-    </div>
-  );
+    return (
+        <div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Carousel/>
+            </Suspense>
+        </div>
+    );
 };
 
 export default CarouselPage;
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,37 @@
+"use client"
+
 import {Carousel as ResponsiveCarousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Carousel.module.css';
 import Image from 'next/image';
+import {redirect, useSearchParams} from "next/navigation";
+import {useEffect, useState} from "react";
+import places from "@/config/placesConfig";
 
-interface CarouselProps {
-    images: string[];
-    initialIndex: number;
-    onClose: () => void;
-}
+const Carousel = () => {
+    const searchParams = useSearchParams();
+    const place = searchParams.get('place');
+    const index = searchParams.get('index');
+    const [initialIndex, setInitialIndex] = useState(0);
+    const [images, setImages] = useState<string[]>([]);
 
-const Carousel = ({images, initialIndex, onClose}: CarouselProps) => {
+    useEffect(() => {
+        if (place && index !== undefined) {
+            const selectedPlace = places.find(p => p.name === place);
+            if (selectedPlace) {
+                setInitialIndex(Number(index));
+                setImages(selectedPlace.webcams.map(webcam => webcam.url));
+            }
+        }
+    }, [place, index]);
+
+    const handleClose = () => {
+        redirect('/');
+    };
+
     return (
         <div className={styles.carouselContainer}>
-            <button onClick={onClose} className={styles.closeButton}>Close</button>
+            <button onClick={handleClose} className={styles.closeButton}>Close</button>
             <ResponsiveCarousel selectedItem={initialIndex} showThumbs={false} infiniteLoop useKeyboardArrows>
                 {images.map((image, index) => (
                     <div key={index} className={styles.imageContainer}>
