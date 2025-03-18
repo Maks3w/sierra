f92d7a9c0d8cd163d4875e60b30952d5205fbbd4
Maks3w <github.maks3w@virtualplanets.net>
2025-03-18 08:03:07 +0100

    Fix first element of the carousel.
    Just by removing the useEffect

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,57 +1,50 @@
 "use client"
 
-import { Carousel as ResponsiveCarousel } from 'react-responsive-carousel';
+import {Carousel as ResponsiveCarousel} from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './Carousel.module.css';
-import { useSearchParams } from 'next/navigation';
-import {useEffect, useRef, useState} from 'react';
+import {useSearchParams} from 'next/navigation';
+import {useEffect, useRef} from 'react';
 import places from '@/config/placesConfig';
 import Link from 'next/link';
 import WebcamImage from './WebcamImage';
-import {Webcam} from "@/types/Places";
 import screenfull from 'screenfull';
 
 
 const Carousel = () => {
-    const searchParams = useSearchParams();
-    const place = searchParams.get('place');
-    const index = searchParams.get('index');
-    const [initialIndex, setInitialIndex] = useState(0);
-    const [webcams, setWebcams] = useState<Webcam[]>([]);
+  const searchParams = useSearchParams();
+  const place = searchParams.get('place');
+  const index = searchParams.get('index');
 
-    const ref = useRef<HTMLDivElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (screenfull.isEnabled) {
+      // @ts-expect-error type assertion
+      screenfull.request(ref.current);
+    }
+  }, [ref]);
 
-    useEffect(() => {
-        if (place && index !== undefined) {
-            const selectedPlace = places.find(p => p.name === place);
-            if (selectedPlace) {
-                setInitialIndex(Number(index));
-                setWebcams(selectedPlace.webcams);
-            }
-        }
-    }, [place, index]);
+  const selectedPlace = places.find(p => p.name === place);
+  if (!selectedPlace) {
+    return <p>Place not found</p>;
+  }
+  const webcams = selectedPlace.webcams;
+  const initialIndex = Number(index);
 
-    useEffect(() => {
-        if (screenfull.isEnabled) {
-            // @ts-expect-error type assertion
-            screenfull.request(ref.current);
-        }
-    }, []);
-
-    return (
-        <div className={styles.carouselContainer} ref={ref}>
-            <Link href={`/${place}`}>
-                <button className={styles.closeButton}>Close</button>
-            </Link>
-            <ResponsiveCarousel selectedItem={initialIndex} showThumbs={false} infiniteLoop useKeyboardArrows>
-                {webcams.map((webcam, index) => (
-                    <div key={`${place}-${index}`} className={styles.imageContainer}>
-                        <WebcamImage webcam={webcam} className={styles.image}/>
-                    </div>
-                ))}
-            </ResponsiveCarousel>
-        </div>
-    );
+  return (
+    <div className={styles.carouselContainer} ref={ref}>
+      <Link href={`/${place}`}>
+        <button className={styles.closeButton}>Close</button>
+      </Link>
+      <ResponsiveCarousel selectedItem={initialIndex} showThumbs={false} infiniteLoop useKeyboardArrows>
+        {webcams.map((webcam, index) => (
+          <div key={`${place}-${index}`} className={styles.imageContainer}>
+            <WebcamImage webcam={webcam} className={styles.image}/>
+          </div>
+        ))}
+      </ResponsiveCarousel>
+    </div>
+  );
 };
 
 export default Carousel;
