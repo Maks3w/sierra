fe458f114de60bd6c2eca237b17e4d345a206953
Maks3w <github.maks3w@virtualplanets.net>
2025-03-21 09:49:32 +0100

    cs

--- next.config.ts
+++ next.config.ts
@@ -1,61 +1,61 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'infocar.dgt.es',
-                port: '',
-                pathname: '/etraffic/data/camaras/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'infocar.dgt.es',
-                port: '',
-                pathname: '/etraffic/img/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'meteocercedilla.com',
-                port: '',
-                pathname: '/webcams/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'meteosierra.com',
-                port: '',
-                pathname: '/weewx/puerto/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'puertonavacerrada.com',
-                port: '',
-                pathname: '/webcam/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'valdesqui.es',
-                port: '',
-                pathname: '/~cam/**',
-            },
-          {
-            protocol: 'https',
-            hostname: 'www.ventamarcelino.com',
-            port: '',
-            pathname: '/meteo/**',
-          },
-        ],
-    },
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/Navacerrada',
-                permanent: true,
-            },
-        ];
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'infocar.dgt.es',
+        port: '',
+        pathname: '/etraffic/data/camaras/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'infocar.dgt.es',
+        port: '',
+        pathname: '/etraffic/img/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'meteocercedilla.com',
+        port: '',
+        pathname: '/webcams/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'meteosierra.com',
+        port: '',
+        pathname: '/weewx/puerto/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'puertonavacerrada.com',
+        port: '',
+        pathname: '/webcam/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'valdesqui.es',
+        port: '',
+        pathname: '/~cam/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.ventamarcelino.com',
+        port: '',
+        pathname: '/meteo/**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/Navacerrada',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
